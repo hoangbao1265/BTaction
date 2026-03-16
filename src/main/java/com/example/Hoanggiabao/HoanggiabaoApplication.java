56593a9216c358978fed0643b988c1a47d426cd8
hoangbao1265 <hoangbao1265@gmail.com>
2026-03-16 15:19:23 +0700

    first

---
+++ src/main/java/com/example/Hoanggiabao/HoanggiabaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hoanggiabao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoanggiabaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoanggiabaoApplication.class, args);
+	}
+
+}
